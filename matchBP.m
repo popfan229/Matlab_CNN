5abdfee130bbcefb52529aa6f45687a930c7950a
panfan <panfan@cloudscapes.cn>
2016-08-25 23:17:00 +0800

    更新深度学习结果文件

--- matchBP.m
+++ matchBP.m
@@ -13,15 +13,17 @@ clc
 close all
 filePath = '..\RawDataRename\';
 sr = 2000;   %sample rate
-waveloc = xlsread('..\BPresult\WaveLocation');
-pulseNum = xlsread('..\BPresult\pulseNum');
-sbpNum = pulseNum(:,3);
-dbpNum = pulseNum(:,5);
+waveloc = xlsread('WaveLocation.xls');
+pulseNum = xlsread('pulseNum.xls');
+sbpNum = pulseNum(:,10);
+dbpNum = pulseNum(:,11);
 for fileID = 1:270 
     data = csvread([filePath num2str(fileID) '.csv']);
     [cuffPressure, bpIn, bpOut] = datasegment(data);
-    sbpLoc = waveloc(fileID,sbpNum(fileID)+2);
-    dbpLoc = waveloc(fileID,dbpNum(fileID)+2);
-    sbp = cuffPressure(sbpLoc)
-    dbp = cuffPressure(dbpLoc)    
+    sbpLoc = waveloc(fileID,sbpNum(fileID)*2+2);
+    dbpLoc = waveloc(fileID,dbpNum(fileID)*2+2);
+    sbp = cuffPressure(sbpLoc);
+    dbp = cuffPressure(dbpLoc);
+    dp = [sbp dbp];
+    xlswrite(['deepLearnResult.xls'], dp,1, ['B' num2str(fileID+1)]);
 end
