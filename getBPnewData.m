7e82cdda38a2e84e3883731060b1c2d9088248b5
popfan229 <popfan229@gmail.com>
2016-10-08 10:54:08 +0800

    2016-10-8 1，更新subject information

--- getBPnewData.m
+++ getBPnewData.m
@@ -1,5 +1,6 @@
 % read BP values from xls for new 100 subject
 bpdata = [];
+height = [];
 for fileID = 1:100
     file = num2str(fileID);
     if fileID<10
@@ -11,9 +12,19 @@ for fileID = 1:100
     end
 
     [ndata,text,alldata]=xlsread('Clinical Information 100+subjects.xls',sheet);
-    bpdata = [bpdata; ndata(8,:)']   
+    bpdata = [bpdata; ndata(8,:)']
+    height(fileID) = ndata(1,1);
+    weight(fileID) = ndata(2,1);
+    armcir(fileID) = ndata(3,1);
+    age(fileID) = ndata(4,2);
+    gender(fileID) = text(6,2);
 end
 sbpstep = 1:2:length(bpdata);
 dbpstep = 2:2:length(bpdata);
 sbp = bpdata(sbpstep);
 dbp = bpdata(dbpstep);
+height = height';
+weight = weight';
+armcir = armcir';
+age = age';
+gender = gender';
